db759aa8b1bcd549d03cfb20fc695cdda9d10cd7
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-08-25 18:04:47 -0300

    criando model de produtos

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,13 +1,9 @@
 import Sequelize, { Model } from 'sequelize'
 import bcrypt from 'bcrypt'
-
 class User extends Model {
-
     static init(sequelize) {
-
         super.init(
             {
-
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
                 password: Sequelize.VIRTUAL,
